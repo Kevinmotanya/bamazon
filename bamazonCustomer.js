7de4ecc5a079eb91cce95c5f90878ab212c4ec15
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2018-12-17 10:00:41 -0600

    Update bamazonCustomer.js
    customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,15 +15,14 @@ var connection = mysql.createConnection({
 
     //Your password
     password: "Delete123@",
-    database: "bamazon"  
+    database: "bamazon"
 });
 
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function(err) {
+    if (err) throw err;
     console.log("connected as id" + connection.threadId);
-  openSales();
+    openSales();
 });
-
 
 //--Beginning of the Inquirer
 
@@ -43,147 +42,143 @@ function openSales() {
             console.log("Thank you! Come back soon!");
         }
     });
-//--
+    //--
 
-//view products list
-function showInventory() {
+    //view products list
+    function showInventory() {
 
-    // declare var of inventory things
-    var table = new Table({
-        head: ['ItemID', 'Item', 'Department', 'Price', 'Stock'],
-        colWidths: [10, 30, 30, 30, 30]
-    });
+        // declare var of inventory things
+        var table = new Table({
+            head: ['ItemID', 'Item', 'Department', 'Price', 'Stock'],
+            colWidths: [10, 30, 30, 30, 30]
+        });
 
-   displayInventory();
+        displayInventory();
 
-    // products table stores products in an array form and that is how we access the products
-    function displayInventory() {
+        // products table stores products in an array form and that is how we access the products
+        function displayInventory() {
 
-        //making a query connection so as to SELECT from products 
+            //making a query connection so as to SELECT from products 
 
-        connection.query("SELECT * FROM products", function(err, res) {
-            for (var i = 0; i < res.length; i++) {
+            connection.query("SELECT * FROM products", function(err, res) {
+                for (var i = 0; i < res.length; i++) {
 
-                var item_id = res[i].item_id,
-                    product_name = res[i].product_name,
-                    department_name = res[i].department_name,
-                    price = res[i].price,
-                    stock_quantity = res[i].stock_quantity;
+                    var item_id = res[i].item_id,
+                        product_name = res[i].product_name,
+                        department_name = res[i].department_name,
+                        price = res[i].price,
+                        stock_quantity = res[i].stock_quantity;
 
-              table.push(
-                  [item_id, product_name, department_name, price, stock_quantity]
-            );
-          }
-            
-            console.log("YOU NEED WE,VE GOT IT");
-            //outputs table information as a string ""
-            console.log(table.toString());
-            
-            firstPrompt();
+                    table.push(
+                        [item_id, product_name, department_name, price, stock_quantity]
+                    );
+                }
+
+                console.log("YOU NEED WE,VE GOT IT");
+                //outputs table information as a string ""
+                console.log(table.toString());
+
+                firstPrompt();
+            });
+        }
+    }
+
+    //--Inquirer user purchase--
+
+    function firstPrompt() {
+
+        inquirer.prompt([{
+
+            type: "confirm",
+            name: "continue",
+            message: "Would you like to purchase an item?",
+            default: true
+
+        }]).then(function(customer) {
+            if (customer.continue === true) {
+                choicePrompt();
+            } else {
+                console.log("Thank you! Come back soon!");
+            }
         });
     }
-}
 
-//--Inquirer user purchase--
+    //prompt to input the item ID and the DB is able to search for it
+    function choicePrompt() {
 
-function firstPrompt() {
-
-    inquirer.prompt([{
-
-        type: "confirm",
-        name: "continue",
-        message: "Would you like to purchase an item?",
-        default: true
-
-    }]).then(function(customer) {
-        if (customer.continue === true) {
-            choicePrompt();
-        } else {
-            console.log("Thank you! Come back soon!");
-        }
-    });
-}
-
-//prompt to input the item ID and the DB is able to search for it
-function choicePrompt() {
-
-    inquirer.prompt([{
+        inquirer.prompt([{
 
             type: "input",
             name: "inputId",
             message: "Enter item ID to be able to do purchase!",
-        },
-        {
+        }, {
             type: "input",
             name: "inputNumber",
             message: "Enter desired units to purchase!",
 
-        }
-    ]).then(function(customerPurchase) {
+        }]).then(function(customerPurchase) {
 
-        //connect to database to find stock_quantity in database. If user quantity input is greater than stock, decline purchase.
+            //connect to database to find stock_quantity in database. If user quantity input is greater than stock, decline purchase.
 
-        connection.query("SELECT * FROM products WHERE item_id=?", customerPurchase.inputId, function(err, res) {
-            for (var i = 0; i < res.length; i++) {
+            connection.query("SELECT * FROM products WHERE item_id=?", customerPurchase.inputId, function(err, res) {
+                for (var i = 0; i < res.length; i++) {
 
-                if (customerPurchase.inputNumber > res[i].stock_quantity) {
+                    if (customerPurchase.inputNumber > res[i].stock_quantity) {
 
-                   console.log(" Dear customer Sorry!Insufficient quantity!");
-                   
-                    openSales();
+                        console.log(" Dear customer Sorry!Insufficient quantity!");
 
-                } else {
-                    //show the selected item's information
-                    
-                    console.log("You are about to buy:");
-                    console.log("\nItem: " + res[i].product_name);
-                    console.log("\nDepartment: " + res[i].department_name);
-                    console.log("\nThe Price is: " + res[i].price);
-                    console.log("\nQuantity purchased: " + customerPurchase.inputNumber);                    
-                    console.log("\nYour Total is: " + res[i].price * customerPurchase.inputNumber);
-                
+                        openSales();
 
-                    var newStock = (res[i].stock_quantity - customerPurchase.inputNumber);
-                    var purchaseId = (customerPurchase.inputId);
-                    //console.log(newStock);
-                    checkOut(newStock, purchaseId);
+                    } else {
+                        //show the selected item's information
+
+                        console.log("You are about to buy:");
+                        console.log("\nItem: " + res[i].product_name);
+                        console.log("\nDepartment: " + res[i].department_name);
+                        console.log("\nThe Price is: " + res[i].price);
+                        console.log("\nQuantity purchased: " + customerPurchase.inputNumber);
+                        console.log("\nYour Total is: " + res[i].price * customerPurchase.inputNumber);
+
+                        var newStock = (res[i].stock_quantity - customerPurchase.inputNumber);
+                        var purchaseId = (customerPurchase.inputId);
+                        //console.log(newStock);
+                        checkOut(newStock, purchaseId);
+                    }
                 }
+            });
+        });
+    }
+
+    //proceed to checkout
+    function checkOut(newStock, purchaseId) {
+
+        inquirer.prompt([{
+
+            type: "confirm",
+            name: "acceptPurchase",
+            message: "Do you want to proceed with this purchase?",
+            default: true
+                //if customer wants to checkout, continue and run the function customerConfirm
+        }]).then(function(customerConfirm) {
+            //if buyer confirms the intention to purchase the query db and update purchase/stockqty
+            if (customerConfirm.acceptPurchase === true) {
+
+                //After customer checks out, connect to DB and update the s
+
+                connection.query("UPDATE products SET ? WHERE ?", [{
+                    stock_quantity: newStock
+                }, {
+                    item_id: purchaseId
+                }], function(err, res) {});
+
+                console.log("Anything else we can do for you today?");
+                //reprompts bamazon
+                openSales();
+            } else {
+                console.log("Please check with our other locations");
+                // starts bamazon store again
+                openSales();
             }
         });
-    });
-}
-
-//proceed to checkout
-function checkOut(newStock, purchaseId) {
-
-    inquirer.prompt([{
-
-        type: "confirm",
-        name: "acceptPurchase",
-        message: "Do you want to proceed with this purchase?",
-        default: true
-//if customer wants to checkout, continue and run the function customerConfirm
-    }]).then(function(customerConfirm) {
-        //if buyer confirms the intention to purchase the query db and update purchase/stockqty
-        if (customerConfirm.acceptPurchase === true) {
-
-            //After customer checks out, connect to DB and update the s
-
-            connection.query("UPDATE products SET ? WHERE ?", [{
-                stock_quantity: newStock
-            }, {
-                item_id: purchaseId
-            }], function(err, res) {
-            });
-
-            console.log("Anything else we can do for you today?");
-            //reprompts bamazon
-            openSales();
-        } else {
-            console.log("Please check with our other locations");
-            // starts bamazon store again
-            openSales();
-        }
-    });
-}
+    }
 }
